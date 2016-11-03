b534d0d893d59cd7dd8daf93dd5263ad2005d33c
groomycian <groomycian@gmail.com>
2016-11-03 16:19:08 +0300

    * added parsing and saving categories to db

--- make_categories.py
+++ make_categories.py
@@ -1,7 +1,11 @@
+import re
 import urllib.request
+import pprint
 
 from sqlalchemy import create_engine
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import relationship, backref
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy_utils.functions import database_exists
 from sqlalchemy_utils.functions import create_database
@@ -9,27 +13,70 @@ from sqlalchemy_utils.functions import create_database
 from bs4 import BeautifulSoup
 
 from settings import DB_USER, DB_PASSWORD, DB_NAME
+from settings import URL_TO_PARSE
 
 Base = declarative_base()
 
 class Category(Base):
     __tablename__ = 'categories'
     id = Column(Integer, primary_key = True)
-    name =  Column(String(50), index = True)
+    name =  Column(String(255), index = True)
+    parent_id = Column(Integer, ForeignKey('categories.id'))
+
+    categories = relationship(
+        "Category",
+        backref = backref('main_category', remote_side=id),
+        lazy="dynamic"
+    )
 
 
 def get_html(url):
     response = urllib.request.urlopen(url)
     return response.read()
 
+def get_categories(html):
+    soup = BeautifulSoup(html, "html.parser")
+    categories_list = soup.find('ul', class_="children-list")
+    items = categories_list.find_all('li')
+    
+    categories = dict()
+    for i, item in enumerate(items):
+        # //print(item.get_text().strip())
+        matches = re.match(r'(\d{1,2})\.(\d{1,2})?\.?(\d{1,2})?\.?(.*)', item.get_text().strip(), re.S)
+        if (matches and matches.groups()[0] == '2'):
+            print(matches.groups())
+            if (matches.groups()[2] == None):
+                categories[matches.groups()[3]] = []
+                parent_id = matches.groups()[1]
+                parent_name = matches.groups()[3]
+            elif (matches.groups()[1] == parent_id):
+                categories[parent_name].append(matches.groups()[3])
+    
+    return categories
+
+
 def main():
     engine = create_engine(
-        'mysql://{}:{}@localhost/{}'.format(DB_USER, DB_PASSWORD, DB_NAME)
+        'mysql://{}:{}@localhost/{}?charset=utf8'.format(DB_USER, DB_PASSWORD, DB_NAME)
     )
+
     if not database_exists(engine.url):
         create_database(engine.url)
         Base.metadata.create_all(engine)
 
+    Session = sessionmaker(bind = engine)
+    session = Session()
+    
+    categories = get_categories(get_html(URL_TO_PARSE))
+    pprint.pprint(categories)
+    for name in categories:
+        category = Category(name = name)
+        session.add(category)
+        session.flush()
+        for subname in categories[name]:
+            session.add(Category(name = subname, parent_id = category.id))
+    
+    session.commit()
 
 if __name__ == '__main__':
     main()
