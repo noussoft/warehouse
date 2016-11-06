99f54365361f0ac66b93533e200033a8ac374d97
groomycian <groomycian@gmail.com>
2016-11-06 23:07:54 +0300

    * added config

---
+++ config.py
@@ -0,0 +1,19 @@
+import os
+
+class Config(object):
+    DEBUG = False
+
+    CSRF_ENABLED = True
+
+    SECRET_KEY = 'YOUR_RANDOM_SECRET_KEY'
+
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
+class ProductionConfig(Config):
+    DEBUG = False
+
+class DevelopmentConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
