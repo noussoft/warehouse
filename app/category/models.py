b1c1340fa6974998507b84da113b12c64f56f0a5
groomycian <groomycian@gmail.com>
2016-11-10 00:23:17 +0300

    * fixed model typos

--- app/category/models.py
+++ app/category/models.py
@@ -3,13 +3,13 @@ from app.database import db
 class Category(db.Model):
     __tablename__ = 'categories'
 
-    id = Column(db.Integer, primary_key = True)
-    name =  Column(db.String(255), index = True)
-    parent_id = Column(db.Integer, db.ForeignKey('categories.id'))
+    id = db.Column(db.Integer, primary_key = True)
+    name =  db.Column(db.String(255), index = True)
+    parent_id = db.Column(db.Integer, db.ForeignKey('categories.id'))
 
     categories = db.relationship(
         "Category",
-        backref = backref('main_category', remote_side=id),
+        backref = db.backref('main_category', remote_side=id),
         lazy="dynamic"
     )
 
