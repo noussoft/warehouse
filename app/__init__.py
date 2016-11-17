52b62deb3add296fa34231a525247165350b2fa5
groomycian <groomycian@gmail.com>
2016-11-17 23:54:07 +0300

    * added flask_admin, category => warehouse

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,18 @@
 import os
 from flask import Flask
+from flask_admin import Admin
 from flask_migrate import Migrate
+from flask_admin.contrib.sqla import ModelView
+from .warehouse.models import Category, Tenant
 from .database import db
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(os.environ['APP_SETTINGS'])
+
+    admin = Admin(app, template_mode='bootstrap3')
+    admin.add_view(ModelView(Category, db.session))
+    admin.add_view(ModelView(Tenant, db.session))
 
     db.init_app(app)
     migrate = Migrate(app, db)
@@ -20,10 +27,10 @@ def create_app():
         except:
             pass
 
-    import app.category.controllers as category
+    import app.warehouse.controllers as warehouse
     import app.general.controllers as general
 
-    app.register_blueprint(category.module)
+    app.register_blueprint(warehouse.module)
     app.register_blueprint(general.module)
 
     return app
