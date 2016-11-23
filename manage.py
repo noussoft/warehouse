eae5e447511755f2d85b6470171fefb95356caa8
groomycian <groomycian@gmail.com>
2016-11-23 09:36:45 +0300

    * removed duplicated code

--- manage.py
+++ manage.py
@@ -5,7 +5,6 @@ from flask_script import Manager
 from app import create_app
 
 app = create_app()
-app.config.from_object(os.environ['APP_SETTINGS'])
 manager = Manager(app)
 
 if __name__ == '__main__':
