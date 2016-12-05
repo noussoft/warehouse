d306edbdee7f3ed38ba78fa2b5c43da39708eafa
groomycian <groomycian@gmail.com>
2016-12-05 14:47:34 +0300

    * cosmetics

--- config.py
+++ config.py
@@ -10,7 +10,6 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-
 class ProductionConfig(Config):
     DEBUG = False
 
