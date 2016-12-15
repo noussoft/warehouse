48ac8fce7d80156971730892e8e5d059ec53153e
groomycian <groomycian@gmail.com>
2016-12-16 01:09:12 +0300

    * added flask-security for admin area protection

--- app/warehouse/models.py
+++ app/warehouse/models.py
@@ -1,3 +1,4 @@
+from flask.ext.security import UserMixin, RoleMixin
 from app.database import db
 
 categories_tenants_table = db.Table('categories_tenants',
@@ -52,3 +53,21 @@ class JumboImage(db.Model):
 
     id = db.Column(db.Integer, primary_key = True)
     image = db.Column(db.String(255), index = False)
+
+roles_users = db.Table('roles_users',
+        db.Column('user_id', db.Integer(), db.ForeignKey('user.id')),
+        db.Column('role_id', db.Integer(), db.ForeignKey('role.id')))
+
+class Role(db.Model, RoleMixin):
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(80), unique=True)
+    description = db.Column(db.String(255))
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(255), unique=True)
+    password = db.Column(db.String(255))
+    active = db.Column(db.Boolean())
+    confirmed_at = db.Column(db.DateTime())
+    roles = db.relationship('Role', secondary=roles_users,
+                            backref=db.backref('users', lazy='dynamic'))
--- config.py
+++ config.py
@@ -14,8 +14,27 @@ class Config(object):
 
     WHOOSH_BASE = os.path.join(basedir, 'search.db')
 
+    # Flask-Security config
+    SECURITY_URL_PREFIX = "/admin"
+    SECURITY_PASSWORD_HASH = os.environ['SECURITY_PASSWORD_HASH']
+    SECURITY_PASSWORD_SALT = os.environ['SECURITY_PASSWORD_SALT']
+
+    # Flask-Security URLs, overridden because they don't put a / at the end
+    SECURITY_LOGIN_URL = "/login/"
+    SECURITY_LOGOUT_URL = "/logout/"
+
+    SECURITY_POST_LOGIN_VIEW = "/admin/"
+    SECURITY_POST_LOGOUT_VIEW = "/admin/"
+
+    # Flask-Security features
+    SECURITY_REGISTERABLE = True
+    SECURITY_SEND_REGISTER_EMAIL = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
 class ProductionConfig(Config):
     DEBUG = False
+
+
 
 class DevelopmentConfig(Config):
     DEVELOPMENT = True
